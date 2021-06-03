8b630b186a989f8c52b97057199bd719056529e7
root <root@DESKTOP-SLGR74H.localdomain>
2021-06-02 23:37:41 -0300

    memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,18 +12,16 @@
 
 #include "libft.h"
 
+
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*S1;
 	unsigned char	*S2;
-	unsigned int	i;
 
-	i = 1;
 	S1 = (unsigned char *)s1;
 	S2 = (unsigned char *)s2;
-	if (n == 0)
-		return (0);
-	while (S1[i] == S2[i] && n > i)
-		i++;
-	return ((int)(*S1 - *S2));
+	while (n-- > 0)
+		if(*S1++ != *S2++)
+			return ((int)(*S1 - *S2));
+	return (0);
 }
