fc42a570ef492032262a660b1abd3a352089f249
root <root@DESKTOP-SLGR74H.localdomain>
2021-06-03 00:11:17 -0300

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -15,10 +15,16 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	unsigned int	i;
+	char *dest;
 
 	i = 0;
+	if (src == 0)
+		return (0);
+	if (dstsize < 1)
+		return (dst);
 	while (dst[i] != '\0')
 		i++;
-	ft_strlcpy(dst[i], src, dstsize - ft_strlen(dst));
+	dest = &dst[i];
+	ft_strlcpy(dest, src, dstsize - ft_strlen(dst));
 	return (ft_strlen(dst));
 }
