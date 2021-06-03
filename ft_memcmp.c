74a0016959a514417a6184f093a3c31bcc6e9600
root <root@DESKTOP-SLGR74H.localdomain>
2021-06-02 23:22:10 -0300

    memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -19,16 +19,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	S1 = (unsigned char *)s1;
 	S2 = (unsigned char *)s2;
-	if (n > 0)
-	{
-		while (*S1 == *S2 && n > 0)
-		{
-			S1++;
-			S2++;
-			n--;
-		}
-		return ((int)(*S1 - *S2));
-	}
-	else
+	if (n == 0)
 		return (0);
+	while (*S1 == *S2 && n-- > 0)
+	{
+		S1++;
+		S2++;
+	}
+	return ((int)(*S1-- - *S2--));
 }
