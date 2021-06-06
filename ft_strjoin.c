4e674aa4976f6f9f27d5f8a2b537ba8a0e35260d
root <root@DESKTOP-SLGR74H.localdomain>
2021-06-06 18:17:51 -0300

    Partes 1 e 2

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: aruth-ra <aruth-ra@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 22:31:34 by aruth-ra          #+#    #+#             */
-/*   Updated: 2021/06/06 18:12:30 by aruth-ra         ###   ########.fr       */
+/*   Updated: 2021/06/06 18:17:19 by aruth-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*join;
 	char	*join2;
 
-	join = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)));
+	join = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!join)
 		return (0);
 	join2 = join + ft_strlen(s1);
 	ft_memcpy(join, s1, ft_strlen(s1));
 	ft_memcpy(join2, s2, ft_strlen(s2));
-	free(join);
+	join[ft_strlen(join)] = 0;
 	return (join);
 }
