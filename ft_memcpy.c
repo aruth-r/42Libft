66234032fda3956dbf0e81ceb923c34bb8ea4d55
root <root@DESKTOP-SLGR74H.localdomain>
2021-06-08 20:55:09 -0300

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: aruth-ra <aruth-ra@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 12:32:09 by aruth-ra          #+#    #+#             */
-/*   Updated: 2021/06/08 20:51:50 by aruth-ra         ###   ########.fr       */
+/*   Updated: 2021/06/08 20:55:01 by aruth-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	destchar = dest;
 	srcchar = src;
-	if (n == 0)
-		return (0);
 	while (n-- > 0)
 		*destchar++ = *srcchar++;
 	return (dest);
