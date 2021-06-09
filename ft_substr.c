ac1774a6e9e5dc9b1c4cd8b7a50873a23d8f5a6b
root <root@DESKTOP-SLGR74H.localdomain>
2021-06-08 22:31:21 -0300

    memcpy

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aruth-ra <aruth-ra@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 22:07:39 by aruth-ra          #+#    #+#             */
-/*   Updated: 2021/06/08 22:17:53 by aruth-ra         ###   ########.fr       */
+/*   Updated: 2021/06/08 22:31:11 by aruth-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	st = (char *)s + start;
 	sub = (char *)malloc(sizeof(char) * len + 1);
+	if(sub == 0)
+		return (0);
 	if (start > ft_strlen(s))
+		{
+			sub[0] = '\0';
+			return sub;
+		}
 		return (0);
 	if (!sub)
 		return (0);
-	ft_memcpy(sub, st, len + 1);
+	ft_memcpy(sub, st, len);
 	sub[len] = 0;
 	return (sub);
 }
