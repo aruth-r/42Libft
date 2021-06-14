c00b7a24770e2e3832afd5bba7db44d03c98321c
root <root@DESKTOP-SLGR74H.localdomain>
2021-06-14 20:52:25 -0300

    ajustes

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aruth-ra <aruth-ra@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 16:47:31 by aruth-ra          #+#    #+#             */
-/*   Updated: 2021/06/14 20:43:16 by aruth-ra         ###   ########.fr       */
+/*   Updated: 2021/06/14 20:52:14 by aruth-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strtrim(char const *s, char const *set)
 	char	*trim;
 
 	if (s == 0 || set == 0)
-		return(0);
+		return (0);
 	if (*s == 0 )
 	{
 		trim = (char *)ft_calloc(1, sizeof(char));
