7200c751ffbf1ff86d979784dd0f4bd6da86b637
root <root@DESKTOP-SLGR74H.localdomain>
2021-06-14 20:59:42 -0300

    ajustes

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: aruth-ra <aruth-ra@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 23:51:41 by aruth-ra          #+#    #+#             */
-/*   Updated: 2021/06/14 20:55:27 by aruth-ra         ###   ########.fr       */
+/*   Updated: 2021/06/14 20:58:58 by aruth-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	ft_putendl_fd(char *s, int fd)
 {
 	if (s != 0)
-		{
-			ft_putstr_fd(s, fd);
-			ft_putchar_fd('\n', fd);
-		}
+	{
+		ft_putstr_fd(s, fd);
+		ft_putchar_fd('\n', fd);
+	}
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: aruth-ra <aruth-ra@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 23:37:50 by aruth-ra          #+#    #+#             */
-/*   Updated: 2021/06/14 20:07:48 by aruth-ra         ###   ########.fr       */
+/*   Updated: 2021/06/14 20:58:17 by aruth-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*string;
 
 	i = 0;
+	if (string == 0)
+		return (0);
 	if (*s == 0)
 	{
 		string = (char *)ft_calloc(1, sizeof(char));
@@ -26,8 +28,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		return (string);
 	}
 	string = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
-	if (string == 0)
-		return (0);
 	while (s[i])
 	{
 		string[i] = f(i, s[i]);
